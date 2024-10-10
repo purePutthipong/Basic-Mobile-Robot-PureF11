59957b3b84b91dfed7a3b377f031b06ed0bba544
purePutthipong <putthipong09.w@gmail.com>
2024-10-10 21:43:57 +0700

    OOP_Led _Pico
    It is that first my life to write case, that i write case to control Led

--- Mobile-Robot/Mobile-Robot.ino
+++ Mobile-Robot/Mobile-Robot.ino
@@ -1,9 +1,14 @@
+#include "Led.h"
+
+Led led1(LED_BUILTIN);
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  led1.on();
+  delay(1000);
+  led1.off();
+  delay(1000);
 }
